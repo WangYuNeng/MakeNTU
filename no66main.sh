360fe8fe7c108a08e9d4a2563c3e95b445023c87
WangYuNeng <33870148+WangYuNeng@users.noreply.github.com>
2018-03-31 21:16:20 +0800

    uncomment some code

--- no66main.sh
+++ no66main.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
+
 #cd
 #cd Documents
 #test -d no66 || mkdir no66
 #cd no66
 #ls
 #utilcode_path='../../Desktop/MakeNTU/make_ntu_no_66-master'
-python3 iot_download.py
 
+test -f csvfile.csv || touch csvfile.csv
+test -d download && rm -rf download
+mkdir download
+echo "Time8" > download/initial.txt
+python3 iot_download.py
 cd download
 #For the first time, filname.txt exists and content = initial
 #head --lines=1 ../filename_new.txt > ../filename_old.txt
 ls -c1 > ../filename_new.txt
 cd ..
-
-#servermain input?
 python3 servermain.py
-#while [ 1==1 ]; do
-#    echo $SECONDS
-#    end=$((SECONDS+3))
-#    while [ $SECONDS -lt $end ]; do
-#        :
-#    done
-#    :
-#done
+while [ 1==1 ]; do
+    python3 iot_download.py
+    cd download
+    ls -c1 > ../filename_new.txt
+    cd ..
+    python3 servermain.py
+    end=$((SECONDS+30))
+    while [ $SECONDS -lt $end ]; do
+        :
+    done
+    :
+done
 
